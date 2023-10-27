b0baf8cffb8906e0fc5d5c168b5fd0f314d53c07
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-27 23:15:26 +0300

    removed useMongoclient due to error

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 const UserModel = require("./model/model");
 
 mongoose.connect("mongodb://127.0.0.1:27017/passport-jwt", {
-  useMongoClient: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 mongoose.connection.on("error", (error) => console.log(error));
 mongoose.Promise = global.Promise;
